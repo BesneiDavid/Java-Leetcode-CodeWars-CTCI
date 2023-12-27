6fbf12428a28c871c8a65e68ee2a350168f2bfdc
BesneiDavid <besneidavid92@yahoo.com>
2023-12-27 22:17:07 +0200

    Condensed all the resolved problems into one file

---
+++ Leetcode/LeetcodeProblems/src/TwoSum.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class TwoSum {
+    //    Time complexity: O(n) we iterate only one time through the array and in worst case we traverse the whole array
+//    Space complexity : O(n) we might end up putting in the worst case all the equivalent elements of the array in the hashmap.
+    public static int[] twoSum(int[] nums, int target) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+
+        for(int i=0;i<nums.length;i++){
+            if(map.containsKey(nums[i])){
+                return new int[]{map.get(nums[i]),i};
+            }
+            map.put(target-nums[i],i);
+        }
+        return new int[]{0,0};
+    }
+
+    public static void main(String[] args) {
+        int[] myArray = new int[]{3,3};
+        int[] result = twoSum(myArray,6);
+        System.out.println(Arrays.toString(result));
+    }
+}
